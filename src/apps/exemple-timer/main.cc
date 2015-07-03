ac3e1a9ce037beae69bc382e4f318999e20e870f
Hadrien Croubois <hadrien.croubois@gmail.com>
2015-07-03 17:43:05 -0500

    update: exemple-timer

--- src/apps/exemple-timer/main.cc
+++ src/apps/exemple-timer/main.cc
@@ -8,14 +8,13 @@ int main()
 
 	khronos::timer c1(ios, [&](size_t i, auto& self){ // auto = kronos::timer
 			printf("[C1 - fast][%lu] tick\n", i);
-			return i%10;
+			return i%5;
 		}, boost::posix_time::millisec(100)
 	);
 
 	khronos::timer c2(ios, [&](size_t i, auto& self){ // auto = kronos::timer
-			bool trigger = i%2 == 0;
-			printf("[C2 - slow][%lu] %s\n", i, trigger?"start":"stop");
-			if (trigger) c1.start();
+			printf("[C2 - slow][%lu] tick\n", i);
+			c1.start();
 			return i%10;
 		}, boost::posix_time::millisec(1000)
 	);
